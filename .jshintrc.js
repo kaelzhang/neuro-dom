bfbbe9b10e20c6d632300a249049cebbf5cd1c24
Kael <i@kael.me>
2013-06-07 18:51:36 +0800

    changes default configurations of silly and bullshit jshint

--- .jshintrc.js
+++ .jshintrc.js
@@ -4,13 +4,17 @@ module.exports = {
     eqeqeq: true,
     eqnull: true,
     expr: true,
-    immed: true,
+
+    globalstrict: false,
+
+    // allow immediant function innvocation
+    immed: false,
     noarg: true,
 
     // Never force string to quoting with single or double quotemarks
     quotmark: false,
     smarttabs: true,
-    undef: true,
+    undef: false,
     unused: true,
 
     sub: true,
@@ -18,11 +22,15 @@ module.exports = {
     browser: true,
     es5: true,
 
+    validthis: true,
+
     // Defining each variables with a `var` statement is more convenient for coding
     onevar: false,
 
-    asi: false,
+    asi: true,
     lastsemic: false,
+
+    laxbreak: true,
 
     // Report JSHint errors but not fail the task
     // force: true,
